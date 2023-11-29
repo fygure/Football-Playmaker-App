4529fde893748f076ba8921142d1a9a149880620
fygure <92011471+fygure@users.noreply.github.com>
2023-11-28 23:39:01 -0600

    added right click to hide shape

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { useState, useRef } from 'react';
-import { Stage, Layer, Rect, Circle, Ring, Text } from 'react-konva';
+import { Stage, Layer, Rect, Circle, Ring, Text, Star } from 'react-konva';
 import Konva from 'konva';
 
+///////////////////////////////////////////////////////////////////////
+//HOOKS
+///////////////////////////////////////////////////////////////////////
 // Custom Hook for common shape functionalities
 const useShape = (shapeType, initialPosition, initialColor) => {
-  const shapeRef = useRef();
+  const shapeRef = useRef(null);
   const [color, setColor] = useState(initialColor);
   const [position, setPosition] = useState(initialPosition);
+  const [visibility, setVisibility] = useState(true);
 
-  const handleClick = () => {
-    console.log(shapeRef.current);
+  const handleClick = (e) => {
+    //console.log(shapeRef.current);
     setColor(Konva.Util.getRandomColor());
   };
 
@@ -31,22 +35,53 @@ const useShape = (shapeType, initialPosition, initialColor) => {
     console.log(`${shapeType} to (x:${currentPos.x}, y:${currentPos.y})`);
   };
 
+  const handleDblClick = () => {
+    console.log(`${shapeType} double clicked!`)
+  }
+
+  //Hides shape
+  const handleRightClick = (e) => {
+    e.evt.preventDefault(true);
+    setVisibility(false);
+    //console.log(e.target.getStage());
+    console.log(`${shapeType} Removed`);
+  };
+
   return {
     shapeRef,
     color,
     position,
+    visibility,
     handleClick,
     handleDragStart,
     handleDragEnd,
+    handleDblClick,
+    handleRightClick,
   };
 };
 
+
+///////////////////////////////////////////////////////////////////////
+//SHAPES
+/* 
+  There are also:
+    Wedge
+    Arc
+    Ellipse
+    Image
+    Line
+*/
+///////////////////////////////////////////////////////////////////////
 const MyCircle = () => {
-  const { shapeRef, color, position, handleClick, handleDragStart, handleDragEnd } = useShape(
-    'Circle',
-    { x: window.innerWidth / 2, y: window.innerHeight / 2 },
-    'orange'
-  );
+  const shapeType = 'Circle';
+  const { shapeRef, color, position, visibility,
+    handleClick, handleDragStart, handleDragEnd,
+    handleDblClick, handleRightClick }
+    = useShape(
+      shapeType,
+      { x: window.innerWidth / 2, y: window.innerHeight / 2 },
+      'orange'
+    );
 
   return (
     <Circle
@@ -60,16 +95,23 @@ const MyCircle = () => {
       onClick={handleClick}
       onDragStart={handleDragStart}
       onDragEnd={handleDragEnd}
+      onDblClick={handleDblClick}
+      onContextMenu={handleRightClick}
+      visible={visibility}
     />
   );
 };
 
 const MyRectangle = () => {
-  const { shapeRef, color, position, handleClick, handleDragStart, handleDragEnd } = useShape(
-    'Rectangle',
-    { x: 50, y: 50 },
-    'green'
-  );
+  const shapeType = 'Rectangle';
+  const { shapeRef, color, position, visibility,
+    handleClick, handleDragStart, handleDragEnd,
+    handleDblClick, handleRightClick }
+    = useShape(
+      shapeType,
+      { x: 50, y: 50 },
+      'green'
+    );
 
   return (
     <Rect
@@ -88,16 +130,23 @@ const MyRectangle = () => {
       draggable={true}
       onDragStart={handleDragStart}
       onDragEnd={handleDragEnd}
+      onDblClick={handleDblClick}
+      onContextMenu={handleRightClick}
+      visible={visibility}
     />
   );
 };
 
 const MyRing = () => {
-  const { shapeRef, color, position, handleClick, handleDragStart, handleDragEnd } = useShape(
-    'Ring',
-    { x: 350, y: 80 },
-    'red'
-  );
+  const shapeType = 'Ring';
+  const { shapeRef, color, position, visibility,
+    handleClick, handleDragStart, handleDragEnd,
+    handleDblClick, handleRightClick }
+    = useShape(
+      shapeType,
+      { x: 350, y: 80 },
+      'red'
+    );
 
   return (
     <Ring
@@ -105,25 +154,63 @@ const MyRing = () => {
       y={position.y}
       fill={color}
       ref={shapeRef}
+      shadowBlur={5}
       outerRadius={60}
       innerRadius={40}
       draggable={true}
       onClick={handleClick}
       onDragStart={handleDragStart}
       onDragEnd={handleDragEnd}
+      onDblClick={handleDblClick}
+      onContextMenu={handleRightClick}
+      visible={visibility}
     />
   );
 };
+
+const MyStar = () => {
+  const shapeType = 'Star';
+  const { shapeRef, color, position, visibility,
+    handleClick, handleDragStart, handleDragEnd,
+    handleDblClick, handleRightClick }
+    = useShape(
+      shapeType,
+      { x: 200, y: 200 },
+      'purple'
+    );
+
+  return (
+    <Star
+      x={position.x}
+      y={position.y}
+      outerRadius={60}
+      innerRadius={35}
+      fill={color}
+      ref={shapeRef}
+      draggable={true}
+      onClick={handleClick}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      onDblClick={handleDblClick}
+      shadowBlur={5}
+      stroke={"red"}
+      onContextMenu={handleRightClick}
+      visible={visibility}
+    />
+  );
+}
+
 
 function App() {
   return (
     <>
       <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
-          <Text text="Try interacting with the shapes" />
+          <Text text="Try interacting with the shapes" draggable />
           <MyRectangle />
           <MyCircle />
           <MyRing />
+          <MyStar />
         </Layer>
       </Stage>
     </>
