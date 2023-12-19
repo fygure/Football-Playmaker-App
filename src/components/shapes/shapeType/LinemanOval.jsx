3347065ce64522252e4aabce8b84c5d9ec95b2b7
fygure <92011471+fygure@users.noreply.github.com>
2023-12-19 00:32:56 -0600

    class into functional style

--- src/components/shapes/shapeType/LinemanOval.jsx
+++ src/components/shapes/shapeType/LinemanOval.jsx
@@ -1,74 +1,65 @@
 // LinemanOval.jsx
-// TODO: Here is where we change the on click handler for click through
-import React from 'react';
+import React, { useState } from 'react';
 import { Ellipse } from 'react-konva';
 import ContextMenu from '../../menus/ContextMenu';
 
-class LinemanOval extends React.Component {
-    constructor(props) {
-        super(props);
+const LinemanOval = (props) => {
+    const {
+        shapeRef,
+        position,
+        initialColor,
+        showContextMenu,
+        contextMenuPosition,
+        isSelected,
+        handleOnClick,
+        handleRightClick,
+        handleDeleteClick,
+        handleDragStart,
+        handleDragEnd,
+        handleHideContextMenu,
+        ellipseRadiuses
+    } = props;
 
-        this.handleClick = this.handleClick.bind(this);
-        this.states = [
-            { leftState: 0, rightState: 200 }, // fully orange
-            { leftState: 0, rightState: -1 }, // right fill
-            { leftState: 0, rightState: 1 }, // left fill
-            { leftState: -200, rightState: 1 } // all fill
-        ]
-        this.state = this.states[0];
-        this.index = 0;
-    }
+    const strokeOptions = { color: 'black', strokeWidth: 2 };
 
-    handleClick() {
-        this.index++;
-        if (this.index >= (this.states.length)) {
-            this.index = 0;
-        }
-        this.setState(this.states[this.index]);
-    }
+    const states = [
+        { leftState: 0, rightState: 200 }, // fully orange
+        { leftState: 0, rightState: -1 }, // right fill
+        { leftState: 0, rightState: 1 }, // left fill
+        { leftState: -200, rightState: 1 } // all fill
+    ];
 
-    render() {
-        const {
-            shapeRef,
-            position,
-            initialColor,
-            showContextMenu,
-            contextMenuPosition,
-            isSelected,
-            handleOnClick,
-            handleRightClick,
-            handleDeleteClick,
-            handleDragStart,
-            handleDragEnd,
-            handleHideContextMenu,
-            ellipseRadiuses
-        } = this.props;
+    const [stateIndex, setStateIndex] = useState(0);
+    const [state, setState] = useState(states[stateIndex]);
 
-        //const ellipseRadiuses = { x: 16, y: 12 };
-        const strokeOptions = { color: 'black', strokeWidth: 2 };
-        return (
-            <>
-                <Ellipse
-                    ref={shapeRef}
-                    x={position.x}
-                    y={position.y}
-                    radiusX={ellipseRadiuses.x}
-                    radiusY={ellipseRadiuses.y}
-                    stroke={strokeOptions.color}
-                    strokeWidth={strokeOptions.strokeWidth}
+    const handleClick = () => {
+        const newIndex = (stateIndex + 1) % states.length;
+        setStateIndex(newIndex);
+        setState(states[newIndex]);
+    };
 
-                    onDragStart={handleDragStart}
-                    draggable
-                    onDragEnd={handleDragEnd}
-                    onClick={this.handleClick}
-                    onContextMenu={handleRightClick}
-                    fillLinearGradientStartPoint={{ x: this.state.leftState, y: 0 }}
-                    fillLinearGradientEndPoint={{ x: this.state.rightState, y: 0 }}
-                    fillLinearGradientColorStops={[0, initialColor, 1, 'black']}
-                />
-                {showContextMenu && <ContextMenu position={contextMenuPosition} onDelete={handleDeleteClick} onMouseLeave={handleHideContextMenu} />}
-            </>
-        );
-    }
+    return (
+        <>
+            <Ellipse
+                ref={shapeRef}
+                x={position.x}
+                y={position.y}
+                radiusX={ellipseRadiuses.x}
+                radiusY={ellipseRadiuses.y}
+                stroke={strokeOptions.color}
+                strokeWidth={strokeOptions.strokeWidth}
+                onDragStart={handleDragStart}
+                draggable
+                onDragEnd={handleDragEnd}
+                onClick={handleClick}
+                onContextMenu={handleRightClick}
+                fillLinearGradientStartPoint={{ x: state.leftState, y: 0 }}
+                fillLinearGradientEndPoint={{ x: state.rightState, y: 0 }}
+                fillLinearGradientColorStops={[0, initialColor, 1, 'black']}
+            />
+            {showContextMenu && <ContextMenu position={contextMenuPosition} onDelete={handleDeleteClick} onMouseLeave={handleHideContextMenu} />}
+        </>
+    );
 }
+
 export default LinemanOval;
