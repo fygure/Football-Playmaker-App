29c998d708a2a786c26ad2070bc4c8fdd0dc3fcc
ChrisOosthuizen <chrisoosthuizen@chrisooizensair.attlocal.net>
2023-12-19 15:42:29 -0600

    Added directional click through

--- src/components/shapes/shapeType/LinemanOval.jsx
+++ src/components/shapes/shapeType/LinemanOval.jsx
@@ -1,6 +1,6 @@
 // LinemanOval.jsx
 import React, { useState } from 'react';
-import { Ellipse } from 'react-konva';
+import { Ellipse, Group, Line } from 'react-konva';
 import ContextMenu from '../../menus/ContextMenu';
 
 const LinemanOval = (props) => {
@@ -23,24 +23,41 @@ const LinemanOval = (props) => {
     const strokeOptions = { color: 'black', strokeWidth: 2 };
 
     const states = [
-        { leftState: 0, rightState: 200 }, // fully orange
+        { leftState: 0, rightState: 200}, // fully orange
         { leftState: 0, rightState: -1 }, // right fill
         { leftState: 0, rightState: 1 }, // left fill
         { leftState: -200, rightState: 1 } // all fill
     ];
 
+    const lineStates = [
+        180,
+        270,
+        315,
+        360,
+        405,
+        450,
+    ];
+
     const [stateIndex, setStateIndex] = useState(0);
-    const [state, setState] = useState(states[stateIndex]);
+    const [lineIndex, setLineIndex] = useState(0);
+    const [state, setState] = useState({colorState: states[stateIndex], lineState: lineStates[lineIndex]});
 
     const handleClick = () => {
         const newIndex = (stateIndex + 1) % states.length;
         setStateIndex(newIndex);
-        setState(states[newIndex]);
+        setState({colorState: states[newIndex], lineState: lineStates[lineIndex]});
+    };
+
+    const handleLineClick = () =>{
+        const newLine = (lineIndex + 1) % lineStates.length;
+        setLineIndex(newLine);
+        setState({colorState: states[stateIndex], lineState: lineStates[newLine]})
     };
 
     return (
         <>
-            <Ellipse
+        <Group draggable>
+            <Ellipse 
                 ref={shapeRef}
                 x={position.x}
                 y={position.y}
@@ -49,14 +66,46 @@ const LinemanOval = (props) => {
                 stroke={strokeOptions.color}
                 strokeWidth={strokeOptions.strokeWidth}
                 onDragStart={handleDragStart}
-                draggable
                 onDragEnd={handleDragEnd}
                 onClick={handleClick}
                 onContextMenu={handleRightClick}
-                fillLinearGradientStartPoint={{ x: state.leftState, y: 0 }}
-                fillLinearGradientEndPoint={{ x: state.rightState, y: 0 }}
+                fillLinearGradientStartPoint={{ x: state.colorState.leftState, y: 0 }}
+                fillLinearGradientEndPoint={{ x: state.colorState.rightState, y: 0 }}
                 fillLinearGradientColorStops={[0, initialColor, 1, 'black']}
             />
+            <Line 
+                points={[position.x, position.y+ellipseRadiuses.y, position.x, position.y+ellipseRadiuses.x*1.2]}
+                strokeWidth={strokeOptions.strokeWidth}
+                stroke={'black'}
+            />
+            <Line 
+                points={[position.x, position.y+ellipseRadiuses.x*1.2, position.x+Math.sin(Math.PI/180 *state.lineState)*ellipseRadiuses.x*.55, position.y+ellipseRadiuses.x*1.2+Math.cos(Math.PI/180 *state.lineState)*ellipseRadiuses.x*.55]}
+                strokeWidth={strokeOptions.strokeWidth}
+                stroke={'black'}
+            />
+
+            <Line 
+                points={[position.x+Math.sin(Math.PI/180 *(state.lineState+25))*ellipseRadiuses.x*.55, position.y+ellipseRadiuses.x*1.2+Math.cos(Math.PI/180 *(state.lineState+25))*ellipseRadiuses.x*.55, position.x+Math.sin(Math.PI/180 *(state.lineState-25))*ellipseRadiuses.x*.55, position.y+ellipseRadiuses.x*1.2+Math.cos(Math.PI/180 *(state.lineState-25))*ellipseRadiuses.x*.55 ]}
+                strokeWidth={strokeOptions.strokeWidth}
+                stroke={'black'}
+            />
+
+            <Ellipse 
+             ref={shapeRef}
+             x={position.x}
+             y={position.y+ellipseRadiuses.x*1.2}
+             radiusX={ellipseRadiuses.x/5}
+             radiusY={ellipseRadiuses.x/5}
+             stroke={'black'}
+             fill={'black'}
+             onClick={handleLineClick}
+             strokeWidth={strokeOptions.strokeWidth}
+             onDragStart={handleDragStart}
+             onDragEnd={handleDragEnd}
+             onContextMenu={handleRightClick}
+            />
+            
+            </Group>
             {showContextMenu && <ContextMenu position={contextMenuPosition} onDelete={handleDeleteClick} onMouseLeave={handleHideContextMenu} />}
         </>
     );
