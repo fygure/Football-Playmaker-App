7fca2f14025d23038f84b0e59f016aac3be23851
KevinZheng14815 <kevinzheng14815@gmail.com>
2023-12-26 01:04:22 -0600

    Added window resizing and handle on click

--- src/components/texts/Text.jsx
+++ src/components/texts/Text.jsx
@@ -22,6 +22,47 @@ function Text(props) {
     const [contextMenuPosition, setContextMenuPosition] = useState({ x: 0, y: 0 });
 
 
+    useEffect(() => {
+        const image = imageRef.current;
+        const initialImagePosition = { x: image.x(), y: image.y() };
+        const initialImageSize = { width: image.width(), height: image.height() };
+        const initialRelativePosition = {
+            x: (position.x - initialImagePosition.x) / initialImageSize.width,
+            y: (position.y - initialImagePosition.y) / initialImageSize.height,
+        };
+
+
+        const handleResize = () => {
+            const newImagePosition = { x: image.x(), y: image.y() };
+            const newImageSize = { width: image.width(), height: image.height() };
+            setPosition({
+                x: initialRelativePosition.x * newImageSize.width + newImagePosition.x,
+                y: initialRelativePosition.y * newImageSize.height + newImagePosition.y,
+            });
+        };
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [position, imageRef]);
+
+    
+ 
+    const handleOnClick = () => {
+        // const node = shapeRef.current;
+        //First empty the selectedShapes array
+        setSelectedTexts([]);
+        //Filter the shapes array to grab the shape by the id
+        const selectedText = texts.find(text => text.id === id);
+        console.log('Text Clicked', selectedText);
+        
+        setSelectedTexts([selectedText]);
+        setSelectedTextID(id);
+        console.log('Selected Text ID:', id);
+    }
+
+
 
 
 } 
