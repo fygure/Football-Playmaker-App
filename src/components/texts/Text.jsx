3854c9f3bfa7a7636ddf7d73281daf69b0fda111
KevinZheng14815 <kevinzheng14815@gmail.com>
2023-12-26 21:31:05 -0600

    Finished the Text.jsx

--- src/components/texts/Text.jsx
+++ src/components/texts/Text.jsx
@@ -62,10 +62,67 @@ function Text(props) {
         console.log('Selected Text ID:', id);
     }
 
+    const handleRightClick = (e) => {
+        e.evt.preventDefault();
+        const stage = e.target.getStage();
+        const mousePos = stage.getPointerPosition();
+        setContextMenuPosition({ x: mousePos.x - 20, y: mousePos.y - 15 });
+        setShowContextMenu(true);
+    };
 
+    const handleDeleteClick = () => {
+        setShowContextMenu(false);
+        onDelete(id);
+    };
+
+    const handleDragStart = () => {
+        setShowContextMenu(false);
+    };
+
+    const handleDragEnd = (e) => {
+        //console.log(e.target.position());
+        setPosition(e.target.position());
+        onChange(id, { x: e.target.x(), y: e.target.y() });
+    };
+
+    const handleHideContextMenu = () => {
+        setShowContextMenu(false);
+    }
+
+    const handleTextChange = (newText) => {
+        onChange(id, { text: newText });
+    };
+
+    const dragBoundFunc = (pos) => {
+        const stage = textRef.current.getStage();
+        const { width: stageWidth, height: stageHeight } = stage.size();
+        const text = textRef.current;
+        const box = text.getClientRect(); // get bounding box of the shape
+
+        let x = pos.x;
+        let y = pos.y;
+
+        if (x < 0) {
+            x = 0;
+        } else if (x > stageWidth - box.width) {
+            x = stageWidth - box.width;
+        }
+
+        if (y < 0) {
+            y = 0;
+        } else if (y > stageHeight - box.height) {
+            y = stageHeight - box.height;
+        }
+
+        return {
+            x,
+            y
+        };
+    };
 
 
 } 
+export default Text;
 
 
 
